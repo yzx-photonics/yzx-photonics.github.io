f4fd8be6129580ab25138ca8f41b68e93246844c
longy <longyangjing@gmail.com>
2026-07-03 10:07:01 +0800

    Update YZX Photonics website

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector(".nav-toggle");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    const isOpen = document.body.classList.toggle("nav-open");
+    navToggle.setAttribute("aria-expanded", String(isOpen));
+  });
+}
